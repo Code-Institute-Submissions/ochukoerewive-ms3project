e2925aef00f8fa05e3945fbd6fcee86f737b3b33
ochukoerewive <ochukoerewive@gmail.com>
2021-07-27 08:52:12 +0000

    Add Vehicle Information

--- app.py
+++ app.py
@@ -88,6 +88,10 @@ def logout():
 def home():
     return render_template("home.html")
 
+@app.route("/park")
+def park():
+    return render_template("park.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,9 @@
 
 $(document).ready(function(){
   $('.sidenav').sidenav();
-  
+  });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, options);
-});
-});
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
