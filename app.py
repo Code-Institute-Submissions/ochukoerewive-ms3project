5602e27b61cee916e8536c0d7c2b69546e79eadc
ochukoerewive <ochukoerewive@gmail.com>
2021-08-19 14:45:30 +0000

    Park information

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import os
 from flask import Flask, flash, render_template, url_for, redirect, request, session
 from flask_pymongo import PyMongo
@@ -68,19 +69,18 @@ def login():
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # taking the username from the db
-    username = mongo.db.users.find_one(
-        {"username": session["user"]})["username"]
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
 
     if session["user"]:
         return render_template("profile.html", username=username)
     return redirect(url_for("login"))
 
-# Route for logout
+
 @app.route("/logout")
 def logout():
     # Remove user from session cookies
-    flash("You've been logged out")
-    session.pop("user")
+    flash("You are logged out")
+    session.clear()
     return redirect(url_for("login"))
 
 # Route for home page
@@ -88,37 +88,39 @@ def logout():
 def home():
     return render_template("home.html")
 
-
 # still working on this page
 @app.route("/park", methods=["GET", "POST"])
 def park():
     if request.method == "POST":
-        vehicleinfo = {
+        parking = {
             "lot": request.form.get("lot"),
             "vehicle": request.form.get("vehicle"),
             "model": request.form.get("model"),
             "year": request.form.get("year"),
             "Plate-Number": request.form.get("Plate-Number"),
-            "color": request.form.get("color")
+            "color": request.form.get("color"),
+             "created_by": session["user"]
         }
-        mongo.db.vehicleinfo.insert_one(vehicleinfo)
+        mongo.db.vehicleinfo.insert_one(parking)
         flash("Thank you for using Our Services")
-        return redirect(url_for("profile", username=session["user"]))
-    return render_template("park.html")
+        return redirect(url_for("profile" ))
+
+    vehicleinfo = mongo.db.vehicleinfo.find()
+    return render_template("park.html", vehicleinfo=vehicleinfo)
+    #return render_template("park.html")
 
 
 @app.route("/tasks", methods=["GET", "POST"])
 def tasks():
-    if "user" in session:
-        tasks = mongo.db.vehicleinfo.find()
-        return render_template("tasks.html", vehicleinfo=tasks)
+    tasks = mongo.db.vehicleinfo.find()
+    return render_template("tasks.html", vehicleinfo=tasks)
 
 
 @app.route("/edit_task/<task_id>", methods=["GET","PORT"])
 def edit_task(task_id):
     tasks = mongo.db.vehicleinfo.find_one({"_id": ObjectId(task_id)})
     tasks = mongo.db.vehicleinfo.find()
-    return render_template("edit_task.html", tasks=tasks, vehicleinfo=tasks)
+    return render_template("edit_task.html", tasks=task, vehicleinfo=tasks)
 
 
 if __name__ == "__main__":
